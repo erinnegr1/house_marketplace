221187ef922b06a1abcd1ec61962d66dbfb29460
Erinnegr1 <erinne.grant@gmail.com>
2022-11-01 16:45:19 -0400

    added navbar with icons and conditionals

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,10 +1,23 @@
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Navbar, Explore, ForgotPassword, Offers, Profile, SignIn, SignUp} from './pages/index'
 
 function App() {
   return (
-    <div>
-     <h1>My House App</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Explore />}/>
+        <Route path='/forgot-password' element={ <ForgotPassword />}/>
+        <Route path='/offers' element={ <Offers />}/>
+        <Route path='/sign-in' element={ <SignIn/>}/>
+        <Route path='/profile' element={ <SignIn/>}/>
+        <Route path='/sign-up' element={ <SignUp />}/>
+      </Routes>
+       <Navbar />
+    </Router>
+   
+    </>
   );
 }
 
---
+++ house-marketplace/src/pages/index.jsx
@@ -0,0 +1,7 @@
+export {default as Explore} from './Explore'
+export {default as ForgotPassword} from './ForgotPassword'
+export {default as Offers} from './Offers'
+export {default as Profile} from './Profile'
+export {default as SignIn} from './SignIn'
+export {default as SignUp} from './SignUp'
+export {default as Navbar} from '../components/Navbar'
