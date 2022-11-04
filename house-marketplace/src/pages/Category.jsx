b437eb044a7c76d78c4e4c32d542635916d2c945
Erinnegr1 <erinne.grant@gmail.com>
2022-11-04 10:07:59 -0400

    working on rendering rent listings from firestore to the category page

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -1,41 +1,85 @@
-import React from 'react'
-import { useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom' 
-import { 
-    collection, 
-    getDocs, 
-    query, 
-    where, 
-    orderBy, 
-    limit, 
-    startAfter } from 'firebase/firestore'
-import { db } from '../firebase.config'
-import {toast} from 'react-toastify'
-import Spinner from '../components/Spinner'
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 const Category = () => {
+  const [listing, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [listing, setListings]= useState(null)
-    const [loading, setLoading]= useState(true)
+  const params = useParams();
 
-    const params = useParams()
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get a reference
+        const listingsRef = collection(db, "listings");
 
-    useEffect(()=> {
-        const fetchListings = async () => {
-            try {
+        // create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
 
-            } catch (error) {
+        // when you execute the query you get a snapshot of that data
+        //gets docs (table rows) for that specific query
+        const querySnap = await getDocs(q);
 
-            }
-        }
-        fetchListings()
-    })
+        // A firebase thing - loop through the snapshot, so initialize empty array
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          console.log("categories", doc.data());
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listings");
+      }
+    }
+
+    fetchListings();
+  }, [params.categoryName]);
 
   return (
-    <div>Category</div>
-  )
-}
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
 
+     {loading ? (
+        <Spinner />
+     ) : listings && listings.length > 0 ? (
+        <></>
+     ) : (
+        <p> No Listings For {params.categoryName}</p>
+     )}
 
-export default Category
+      </div>
+    )
+};
+
+export default Category;
